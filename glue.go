1833863ba33e7f2f1cf5f225f7d198d86c203ad5
Jiujiang Yuan <jiujiangyuan@qq.com>
2019-04-20 21:57:09 +0800

    add gorm support

--- glue.go
+++ glue.go
@@ -1,36 +1,46 @@
 package glue
 
 import (
-    "github.com/guyannanfei25/go_common"
-    sj "github.com/guyannanfei25/go-simplejson"
+	"fmt"
+
+	sj "github.com/guyannanfei25/go-simplejson"
+	"github.com/guyannanfei25/go_common"
 )
 
 func Init(conf *sj.Json) error {
-    // init logger
-    logDir     := conf.Get("log_info").Get("dir").MustString("/tmp")
-    logName    := conf.Get("log_info").Get("name").MustString("log.info")
-    logLevel   := conf.Get("log_info").Get("level").MustInt(1)
+	// init logger
+	logDir := conf.Get("log_info").Get("dir").MustString("/tmp")
+	logName := conf.Get("log_info").Get("name").MustString("log.info")
+	logLevel := conf.Get("log_info").Get("level").MustInt(1)
 
-    if err := common.InitLogger(logDir, logName, logLevel); err != nil {
-        return err
-    }
+	if err := common.InitLogger(logDir, logName, logLevel); err != nil {
+		return err
+	}
 
-    // init gc
-    maxMem     := conf.Get("gc_info").Get("max_mem_m").MustInt(2048)
-    checkInter := conf.Get("gc_info").Get("check_interval_s").MustInt(20)
-    
-    go common.IntervalGC(maxMem, checkInter)
+	// init gc
+	maxMem := conf.Get("gc_info").Get("max_mem_m").MustInt(2048)
+	checkInter := conf.Get("gc_info").Get("check_interval_s").MustInt(20)
 
-    // init pid
-    pidName    := conf.Get("pid_info").Get("file").MustString("/tmp/glue.pid")
+	go common.IntervalGC(maxMem, checkInter)
 
-    if err := common.InitPidFile(pidName); err != nil {
-        return err
-    }
+	// init pid
+	pidName := conf.Get("pid_info").Get("file").MustString("/tmp/glue.pid")
 
-    maxProc    := conf.Get("proc_info").Get("max_proc").MustInt(-1)
+	if err := common.InitPidFile(pidName); err != nil {
+		return err
+	}
 
-    common.InitRunProcs(maxProc)
+	maxProc := conf.Get("proc_info").Get("max_proc").MustInt(-1)
 
-    return nil
+	common.InitRunProcs(maxProc)
+
+	dsn := conf.Get("db_conf").Get("dsn").MustString("")
+	if dsn == "" {
+		return fmt.Errorf("db conf dsn is empty")
+	}
+	if err := common.InitGorm(dsn); err != nil {
+		return err
+	}
+
+	return nil
 }
